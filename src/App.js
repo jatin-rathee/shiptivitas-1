41ec708643dd57375047e9d9565a98170afaaa51
jatin-rathee <jatinrathee2018@gmail.com>
2020-06-18 15:18:41 +0530

    completed task

--- src/App.js
+++ src/App.js
@@ -6,49 +6,49 @@ import Board from './Board';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: 'home',
-    };
-  }
-  renderShippingRequests() {
-    return (<Board />);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedTab: 'home',
+		};
+	}
+	renderShippingRequests() {
+		return <Board />;
+	}
 
-  renderNavigation() {
-    return (<Navigation
-      onClick={(tabName) => this.changeTab(tabName)}
-      selectedTab={this.state.selectedTab}
-      />);
-  }
+	renderNavigation() {
+		return (
+			<Navigation
+				onClick={tabName => this.changeTab(tabName)}
+				selectedTab={this.state.selectedTab}
+			/>
+		);
+	}
 
-  renderTabContent() {
-    switch(this.state.selectedTab) {
-      case 'home':
-      default:
-        return HomeTab();
-      case 'shipping-requests':
-        return this.renderShippingRequests();
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        {this.renderNavigation()}
+	renderTabContent() {
+		switch (this.state.selectedTab) {
+			case 'home':
+			default:
+				return HomeTab();
+			case 'shipping-requests':
+				return this.renderShippingRequests();
+		}
+	}
+	render() {
+		return (
+			<div className='App'>
+				{this.renderNavigation()}
 
-        <div className="App-body">
-          {this.renderTabContent()}
-        </div>
-      </div>
-    );
-  }
+				<div className='App-body'>{this.renderTabContent()}</div>
+			</div>
+		);
+	}
 
-  changeTab(tabName) {
-    this.setState({
-      selectedTab: tabName,
-    });
-  }
+	changeTab(tabName) {
+		this.setState({
+			selectedTab: tabName,
+		});
+	}
 }
 
 export default App;
